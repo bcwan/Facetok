78dd34cc26c6e8762a8497b1f78633c65add6304
bcwan <bcwan8@gmail.com>
2021-06-15 19:53:09 -0700

    Connect to MongoDB

---
+++ config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  MONGODB: 'mongodb+srv://brian20:LMx1LMTFM00lmuX7@cluster0.smpkb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+}
--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const { ApolloServer } = require('apollo-server');
 const gql = require('graphql-tag');
+const mongoose = require('mongoose');
+
+const { MONGODB } = require('./config.js');
 
 const typeDefs = gql`
   type Query {
@@ -19,7 +22,13 @@ const server = new ApolloServer({
   resolvers
 });
 
-server.listen({ port: 5000 })
+// connect to database
+mongoose
+  .connect(MONGODB, { useNewUrlParser: true })
+  .then(() => {
+    console.log('MongoDB Connected')
+    return server.listen({ port: 5000 });
+  })
   .then(res => {
     console.log(`Server running at ${res.url}`)
-  })
+  });
