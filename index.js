1125d406740203dbb1e46d99dc4dc913a7106b7b
bcwan <bcwan8@gmail.com>
2021-06-16 22:40:55 -0700

    Add GraphQL typeDefs and resolvers

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
-// 29:10 https://www.youtube.com/watch?v=n1mdAPFq2Os&ab_channel=freeCodeCamp.org
 const { ApolloServer } = require('apollo-server');
 const gql = require('graphql-tag');
 const mongoose = require('mongoose');
 
+const Post = require('./models/Post');
 const { mongoURI } = require('./config.js');
 
 const typeDefs = gql`
+  type Post {
+    id: ID!
+    body: String!
+    createdAt: String!
+    username: String!
+  }
   type Query {
-    sayHi: String!
+    getPosts: [Post]
   }
 `;
 
 // needed for any subscription or mutation in GraphQL
 const resolvers = {
   Query: {
-    sayHi: () => 'Hello World!'
+    async getPosts() {
+      try {
+        const posts = await Post.find();
+        return posts;
+      } catch(err) {
+        throw new Error(err);
+      }
+    }
   }
 }
 
