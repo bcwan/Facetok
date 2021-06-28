7eb25ba2cfd1f55269bec4b49783eaf1e49dc623
bcwan <bcwan8@gmail.com>
2021-06-27 17:03:49 -0700

    Add register mutation

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -1,11 +1,44 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { SECRET_KEY } = require('../../config');
+
 const User = require('../../models/User');
+
+function generateToken(user) {
+  return jwt.sign(
+    {
+      id: user.id,
+      email: user.email,
+      username: user.username
+    },
+    SECRET_KEY,
+    { expiresIn: '1h' }
+  );
+}
 
 module.export = {
   Mutation: {
-    register(_, args, context, info) {
-      // Validate user data
-      // Make sure user doesn't exist
-      // Hash password and create user auth token
+    async register(_, { registerInput: { username, email, password, confirmPassword }}) {
+              // Validate user data
+              // Make sure user doesn't exist
+              // Hash password and create user auth token 51:26
+              password = await bcrypt.hash(password, 12);
+              const newUser = new User({
+                email,
+                username,
+                password,
+                createdAt: new Date().toISOString()
+              });
+
+              const res = await newUser.save();
+
+              const token = generateToken(res);
+              
+              return {
+                ...res._doc,
+                id: res._id,
+                token
+              };
+      }
     }
-  }
-}
+};
