4bde01abccd183c809edbc5a087d30251b86b757
bcwan <bcwan8@gmail.com>
2021-07-10 23:14:33 -0700

    Create createPost GraphQL resolver

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,10 +1,11 @@
 const Post = require('../../models/Post');
+const checkAuth = require('../../util/check-auth');
 
 module.exports = {
   Query: {
     async getPosts() {
       try {
-        const posts = await Post.find();
+        const posts = await Post.find().sort({ createdAt: -1 });
         return posts;
       } catch(err) {
         throw new Error(err);
@@ -25,7 +26,18 @@ module.exports = {
   },
   Mutation: {
     async createPost(_, { body }, context) {
-      
+      const user = checkAuth(context);
+
+      const newPost = new Post({
+        body,
+        user: user.id,
+        username: user.username,
+        createdAt: new Date().toISOString()
+      });
+
+      const post = await newPost.save();
+
+      return post;
     }
   }
 }
