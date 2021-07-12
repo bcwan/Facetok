6afd5372918a656f1058dba8212ba440b16d5825
bcwan <bcwan8@gmail.com>
2021-07-11 22:39:49 -0700

    Add deleteComment endpoint

--- graphql/resolvers/comments.js
+++ graphql/resolvers/comments.js
@@ -1,6 +1,6 @@
 const Post = require('../../models/Post');
 const authHeader = require('../../util/check-auth');
-const { UserInputError } = require('apollo-server');
+const { UserInputError, AuthenticationError } = require('apollo-server');
 
 module.exports = {
   Mutation: {
@@ -25,6 +25,25 @@ module.exports = {
         await post.save();
         return post;
       } else throw new UserInputError('Post not found');
+    },
+    async deleteComment(_, { postId, commentId }, context) {
+      const { username } = authHeader(context);
+
+      const post = await Post.findById(postId);
+
+      if (post) {
+        const commentIndex = post.comments.findIndex((c) => c.id === commentId);
+
+        if (post.comments[commentIndex].username === username) {
+          post.comments.splice(commentIndex, 1);
+          await post.save();
+          return post;
+        } else {
+          throw new AuthenticationError('Action not allowed');
+        }
+      } else {
+        throw new UserInputError('Post not found!');
+      }
     }
   }
 }
