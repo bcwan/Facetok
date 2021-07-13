0e7a708d3b8df863e039ba540b36c4a67e92184a
bcwan <bcwan8@gmail.com>
2021-07-13 13:32:16 -0700

    Remove comments

--- util/check-auth.js
+++ util/check-auth.js
@@ -7,7 +7,6 @@ module.exports = (context) => {
   // context = { ...headers }
   const authHeader = context.req.headers.authorization;
   if (authHeader) {
-    // Bearer ...
     const token = authHeader.split('Bearer ')[1];
     if (token) {
       try {
